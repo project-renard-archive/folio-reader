6b0ad86853a0cd075808ad30ffcaecb71e6a2517
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-03-25 23:18:53 -0500

    remove extra docview attribute

--- lib/Folio/Viewer/Component/DocView/Null.pm
+++ lib/Folio/Viewer/Component/DocView/Null.pm
@@ -5,8 +5,6 @@ use warnings;
 use Moo;
 with qw(Folio::Viewer::Component::DocView::CanvasManagerRole);
 
-has docview => ( is => 'rw', weak_ref => 1 );
-
 sub load {
 	my ($self) = @_;
 	$self->docview->_widgets->{cv}->configure(
